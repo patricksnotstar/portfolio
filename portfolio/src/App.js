f61163e6a11849f39bcbac73007730d76199c247
Patrick Nguyen <patricknguyen97@gmail.com>
2023-11-05 19:24:29 -0800

    fixed tailwind config

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <div className="flex w-screen h-screen bg-[#1E1C1C] text-white justify-center place-items-center relative text">
             <div className="flex flex-col w-screen h-full p-8 items-start">
                 <Container userRequest={input} />
-                <div className="flex absolute bottom-0 left-0 pb-8 pl-8 w-3/5 pt-10">
+                <div className="flex absolute bottom-0 left-0 pb-8 pl-8 w-3/5 pt-10 block">
                     {"$"}&nbsp;
                     <Input userInputCallback={(command) => userInput(command)} />
                 </div>
